f2d49da14f0f3e017fd44e76e9e10e2c5f19d0d9
Lauren <scps940327@gmail.com>
2019-05-13 17:45:10 +0800

    【更新】logo掉圖、響應式宣告
    - 解決 header logo 相對路徑掉圖問題
    - 補上網頁<head></head>響應式宣告以方便手機操作

--- src/js/App.js
+++ src/js/App.js
@@ -13,7 +13,7 @@ function App() {
         <div className="max_width">
           <div className="row">
             <div className="col-auto">
-              <img src="../../docs/img/logo.png" width="150px"/>
+              <img src="./img/logo.png" width="150px"/>
             </div>
             <div className="col">
                <ul className="row justify-content-md-end">
